b93e4ec6675e3f3f11d4665fbbef1db5ce4863b7
qin <qmmq182@outlook.com>
2023-06-27 23:30:23 +0800

    Deal with non-existent order_id in parents parameter

--- src/workflow.ts
+++ src/workflow.ts
@@ -6,6 +6,7 @@ class Order {
     orderId: string;
     parentIdList: string[];
     node: Node | undefined;
+    nonExistentParentIdList: string[] = [];
 
     constructor(orderId: string, parentIdList=[]) {
         this.orderId = orderId;
@@ -22,18 +23,39 @@ class Order {
     }
 
     getEdges(orderId2OrderMap: Map<string, Order>) {
-        let edges: Edge[] = []
+        let edges: Edge[] = [];
         for (let parentOrderId of this.parentIdList) {
             let aParentOrder: Order | undefined = orderId2OrderMap.get(parentOrderId);
             if (aParentOrder) {
                 edges.push(
                     new Edge([aParentOrder.getNode(), this.getNode()], {})
                 );
+            } else {
+                // Sometimes we have typo that the order_id in "parents" patermeter does not exist
+                this.nonExistentParentIdList.push(parentOrderId);
             }
         }
         return edges;
     }
     
+}
+
+class NonExistentOrder extends Order {
+    
+    constructor(orderId: string, parentIdList=[]) {
+        super(orderId, parentIdList);
+        this.orderId = orderId + " (does not exist)";
+    }
+
+    getNode() {
+        if (this.node === undefined) {
+            this.node = new Node(this.orderId, {
+                [_.shape]: 'box',
+                [_.color]: 'red'
+            });
+        }
+        return this.node;
+    }
 }
 
 
@@ -66,6 +88,17 @@ export function convertEmfWorkflowToDot(text: string) {
         }
         
     }
+
+    // Deal with non-existent order_id in parents parameter:
+    for (let order of orders) {
+        for (let nonExistentParentOrderId of order.nonExistentParentIdList) {
+            let nonExistentParentOrder = new NonExistentOrder(nonExistentParentOrderId);
+            G.addNode(nonExistentParentOrder.getNode());
+            let nonExistentEdge = new Edge([nonExistentParentOrder.getNode(), order.getNode()], {});
+            G.addEdge(nonExistentEdge);
+        }
+    }
+
     const dot = toDot(G);
     return dot;
 
